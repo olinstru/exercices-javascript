6b6c75f550817b8965338eab39fbb6c058eb8e89
olinstru <o.linstrumelle@betclicgroup.com>
2023-08-18 14:53:48 +0200

    exercice 3

---
+++ Exercice3/script.js
@@ -0,0 +1,34 @@
+/* Chaîne de caractères (String) :
+
+Explication : Une chaîne de caractères est du texte, comme des mots ou des phrases.
+Exemple : */ "Bonjour tout le monde !" /* est une chaîne de caractères.
+
+
+ombre (Number) :
+
+Explication : Un nombre est un chiffre. Ça peut être un nombre entier ou avec des décimales.
+Exemple : */ 42 ou 3.14 /* sont des nombres.
+
+
+Booléen (Boolean) :
+
+Explication : Un booléen est comme un interrupteur, soit "vrai" (true), soit "faux" (false).
+Exemple : */ true ou false /* sont des booléens.
+
+
+Tableau (Array) :
+
+Explication : Un tableau est une liste de choses. Cela peut être des nombres, des textes, ou d'autres choses.
+Exemple : */ let couleurs = ["rouge", "vert", "bleu"] /* est un tableau de couleurs.
+
+
+Objet (Object) :
+
+Explication : Un objet est comme un sac à dos où vous pouvez mettre différentes choses et les étiqueter. Chaque chose a un nom et une valeur.
+Exemple : */
+let personne = {
+  prenom: "Alice",
+  age: 25,
+  estEtudiant: true
+};
+/* Ici, personne est un objet avec des étiquettes comme prenom, age et estEtudiant, chacune ayant sa propre information.
