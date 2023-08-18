2d9bb104a1ffc9f27b0c2a81325a94fcb34dd7d4
olinstru <o.linstrumelle@betclicgroup.com>
2023-08-18 12:07:24 +0200

    exercice 1

---
+++ Exercice1/script.js
@@ -0,0 +1,6 @@
+var nb1 = parseInt(prompt("Saisissez un premier chiffre"));
+var nb2 = parseInt(prompt("Saisissez un deuxième chiffre"));
+var nb3 = parseInt(prompt("Saisissez un troisième chiffre"));
+
+moyenne = (nb1+nb2+nb3)/3;
+alert("La moyenne de ces trois chiffres est : " + moyenne);
