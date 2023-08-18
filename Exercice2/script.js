f5f68f65374052686a2a2d29b1063a6ff84733dd
olinstru <o.linstrumelle@betclicgroup.com>
2023-08-18 14:32:30 +0200

    exercice 2

---
+++ Exercice2/script.js
@@ -0,0 +1,14 @@
+/* // Code Snippet
+let x = 5;
+console.log("The value of x is: " + x);
+y = 10;
+console.log("The value of y is: " + y);
+*/
+
+// Corrected version
+let x = 5;
+console.log("The value of x is: " + x);
+let y = 10; // Declare y using 'let'
+console.log("The value of y is: " + y);
+
+// The fixed code enables us to see the value of x and y in the console log.
