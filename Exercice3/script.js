081689dabda50a8083fc93060ccb4bfb7a229bbb
olinstru <o.linstrumelle@betclicgroup.com>
2023-08-18 14:59:08 +0200

    modifications commentaires js

--- Exercice3/script.js
+++ Exercice3/script.js
@@ -1,34 +1,46 @@
 /* Chaîne de caractères (String) :
 
-Explication : Une chaîne de caractères est du texte, comme des mots ou des phrases.
-Exemple : */ "Bonjour tout le monde !" /* est une chaîne de caractères.
+Explication : Une chaîne de caractères est du texte, comme des mots ou des phrases.*/
+// Exemple :
+"Bonjour tout le monde !"
+// est une chaîne de caractères.
 
 
-ombre (Number) :
+/* Nombre (Number) :
 
-Explication : Un nombre est un chiffre. Ça peut être un nombre entier ou avec des décimales.
-Exemple : */ 42 ou 3.14 /* sont des nombres.
-
-
-Booléen (Boolean) :
-
-Explication : Un booléen est comme un interrupteur, soit "vrai" (true), soit "faux" (false).
-Exemple : */ true ou false /* sont des booléens.
+Explication : Un nombre est un chiffre. Ça peut être un nombre entier ou avec des décimales.*/
+// Exemple :
+42 
+// ou 
+3.14 
+// sont des nombres.
 
 
-Tableau (Array) :
+/* Booléen (Boolean) :
 
-Explication : Un tableau est une liste de choses. Cela peut être des nombres, des textes, ou d'autres choses.
-Exemple : */ let couleurs = ["rouge", "vert", "bleu"] /* est un tableau de couleurs.
+Explication : Un booléen est comme un interrupteur, soit "vrai" (true), soit "faux" (false).*/ 
+// Exemple :
+true 
+// ou 
+false
+// sont des booléens.
 
 
-Objet (Object) :
+/* Tableau (Array) :
 
-Explication : Un objet est comme un sac à dos où vous pouvez mettre différentes choses et les étiqueter. Chaque chose a un nom et une valeur.
-Exemple : */
+Explication : Un tableau est une liste de choses. Cela peut être des nombres, des textes, ou d'autres choses.*/
+// Exemple :
+let couleurs = ["rouge", "vert", "bleu"]
+// est un tableau de couleurs.
+
+
+/* Objet (Object) :
+
+Explication : Un objet est comme un sac à dos où vous pouvez mettre différentes choses et les étiqueter. Chaque chose a un nom et une valeur.*/
+// Exemple :
 let personne = {
   prenom: "Alice",
   age: 25,
   estEtudiant: true
 };
-/* Ici, personne est un objet avec des étiquettes comme prenom, age et estEtudiant, chacune ayant sa propre information.
+// Ici, personne est un objet avec des étiquettes comme prenom, age et estEtudiant, chacune ayant sa propre information.
